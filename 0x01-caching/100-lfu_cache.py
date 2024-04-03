bdd91e856694db63e7456be0b91d00e87ff710ca
GideonBature <infoaboutgideon@gmail.com>
2024-04-03 20:20:09 +0100

    a class LFUCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/100-lfu_cache.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+"""5. LFU Caching
+"""
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LFUCache(BaseCaching):
+    def __init__(self):
+        """init method
+        """
+        super().__init__()
+        self.cache_data = OrderedDict()
+        self.usage_counts = {}
+        self.least_freq_used = None
+
+    def put(self, key, item):
+        """adds cache to cache data
+        """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.usage_counts[key] += 1
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                self.evict()
+            self.cache_data[key] = item
+            self.usage_counts[key] = 1
+
+        self.update_least_freq_used()
+
+    def get(self, key):
+        """retrieves a cache data
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        self.usage_counts[key] += 1
+        self.update_least_freq_used()
+        return self.cache_data[key]
+
+    def evict(self):
+        """check for cache data used less frequently
+        """
+        if not self.cache_data:
+            return
+
+        least_used = min(self.usage_counts.values())
+        candidates = [k for k, v in self.usage_counts.items()
+                      if v == least_used]
+
+        if len(candidates) > 1:
+            # Apply LRU policy among the candidates
+            for key in self.cache_data:
+                if key in candidates:
+                    evict_key = key
+                    break
+        else:
+            evict_key = candidates[0]
+
+        print(f'DISCARD: {evict_key}')
+        del self.cache_data[evict_key]
+        del self.usage_counts[evict_key]
+
+    def update_least_freq_used(self):
+        """updates the number of frequency used
+        """
+        least_used = min(self.usage_counts.values())
+        self.least_freq_used = [k for k, v in self.usage_counts.items()
+                                if v == least_used]
